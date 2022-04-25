415105e581d7c039c881e5caace0863d4709b72a
Ubuntu <ubuntu@ip-172-31-46-197.ap-northeast-2.compute.internal>
2022-04-25 01:56:47 +0000

    html 템플릿 추가

--- main.py
+++ main.py
@@ -1,11 +1,13 @@
 from flask import Flask, request, render_template, redirect, url_for
 app = Flask(__name__, static_folder='./static/')
 
-
 @app.route('/', methods=['GET'])
-def page1():
-    return render_template('1.html')
+def main():
+    return render_template('main.html')
 
+@app.route('/<int:page>', methods=['GET'])
+def story(page):
+    return render_template(f'page{page}.html')
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', debug=True)
+    app.run(host='172.31.46.197', debug=True, port=80)
